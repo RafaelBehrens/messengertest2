a3e8a0d028b44617db480c74c2cf6fc4ea6a8545
RafaelBehrens <Rafabehrens@aol.com>
2016-09-18 12:11:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var app = express();
 var classes = require('resources/classes.json');
+var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
