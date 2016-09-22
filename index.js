1fe43107059f4c6e6eef91ba32e3bd1f2ffa7010
RafaelBehrens <Rafabehrens@aol.com>
2016-09-22 22:00:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
-var classes = fs.readFileSync('classes.json', 'utf8');
+var classes = fs.readFileSync('classes.txt', 'utf8');
 
 // Server frontpage
 app.get('/', function (req, res) {
