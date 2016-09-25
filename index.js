0994d51c1bd5c6792b9dbb68c6800ba478171649
RafaelBehrens <Rafabehrens@aol.com>
2016-09-25 22:15:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var fs = require('fs');
-var cronJob = require('cron').Cronjob;
+var cronJob = require('cron').CronJob;
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
