645ad7f8256eed6c762a6cf098800599c23be268
RafaelBehrens <Rafabehrens@aol.com>
2016-10-02 17:37:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -200,9 +200,9 @@ function classdatasend(recipientId) {
 
 }
 
-//var fedesenderid = 1210619582313639;
+var fedesenderid = 1210619582313639;
 
-//sendMessage(fedesenderid, "Hello Fede");
+sendMessage(fedesenderid, {text: "Hello Fede"));
 
 /*
 var job = new CronJob({
