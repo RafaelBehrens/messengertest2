28a49240c6e5b1c5217c380231c0b098d0ffebf4
RafaelBehrens <Rafabehrens@aol.com>
2016-10-04 22:14:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,59 +5,13 @@ var fs = require('fs');
 var CronJob = require('cron').CronJob;
 var app = express();
 var pg = require('pg');
+var moment = require('moment');
+var classes;
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
-
-//connect to PostGres database
-/*pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-client
-    .query('CREATE TABLE items(id SERIAL PRIMARY KEY, senderid BIGINT, complete BOOLEAN)')
-	.on('row', function(row) {
-      	console.log(JSON.stringify(row));
-    });
-    
-const connectionString = process.env.DATABASE_URL;
-
-const client = new pg.Client(connectionString);
-
-client.connect();
-
-var query = client.query('DROP table items');   
-query.on("end", function (result) {          
-            client.end(); 
-            console.log('items table destroyed');  
-});
-
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, senderid BIGINT, complete BOOLEAN)');   
-query.on("end", function (result) {          
-            client.end(); 
-            console.log('items table created');  
-        });*/
-
-
-//url for classes JSON
-var url = 'https://yogaia.com/api/lessons?upcoming=0&limit=10';
-
-//get JSON, parse it and store it in classes variable
-request(url, (error, response, body)=> {
-  if (!error && response.statusCode === 200) {
-    classes = JSON.parse(body)
-    console.log("Got a response")
-  } else {
-    console.log("Got an error: ", error, ", status code: ", response.statusCode)
-  }
-})
 
 // Server frontpage
 app.get('/', function (req, res) {
@@ -78,29 +32,21 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        
-        if (event.message && event.message.text) {
-            classdatasend(event.sender.id);
-	    
-            sendMessage(event.sender.id, {text: "Hello Fede"});
+  		if (event.postback) {
+            console.log("Postback received: " + JSON.stringify(event.postback));
+            console.log(event.sender.id);
             const connectionString = process.env.DATABASE_URL;
 	    
 	       	const client = new pg.Client(connectionString);
  		
-			/*client.connect();
+			client.connect();
 			
 			var query = client.query("insert into items (senderid) values ('" + event.sender.id + "')");    
         	query.on("end", function (result) {          
             	client.end(); 
             	console.log('SenderID inserted');
         	});
-        	var query = client.query("SELECT senderid from items");
-        	query.on("row", function (row){
-        	    console.log(JSON.stringify(row.senderid));
-        	});*/
-            
-        } else if (event.postback) {
-            console.log("Postback received: " + JSON.stringify(event.postback));
+            sendMessage(event.sender.id, {text: "Great to have you on board! I'll message you daily at around 8am GMT with some upcoming live classes, namaste!"});
         }
     }
     res.sendStatus(200);
@@ -125,75 +71,59 @@ function sendMessage(recipientId, message) {
     });
 };
 
-function setGreeting() {
-	request({
-        url: 'https://graph.facebook.com/v2.6/1119887924743051/thread_settings',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            "setting_type": "greeting",
-            "greeting":{
-            	"text": "Hi {{user_first_name}}, I'm a prototype bot by Yoga.ai. I'm currently a bit unsophisticated, but I'll try and let you know the day's upcoming live classes."
-            }
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
-
-setGreeting();
-
-//add get started button for first use
-/*function setStartButton() {
-	request({
-        url: 'https://graph.facebook.com/v2.6/1119887924743051/thread_settings',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            "setting_type":"call_to_actions",
-  			"thread_state":"new_thread",
-  			"call_to_actions":[{
-      			"payload":"USER_DEFINED_PAYLOAD"
-    		}]
-            }
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};*/
-
-
+function requestclasses(recipientId) {
+	//url for classes JSON
+	var url = 'https://yogaia.com/api/lessons?upcoming=1&limit=30';
+	//get JSON, parse it and store it in classes variable
+	request(url, (error, response, body)=> {
+  		if (!error && response.statusCode === 200) {
+    		classes = JSON.parse(body);
+    		console.log('Sending class data to users...');
+   			const connectionString = process.env.DATABASE_URL;
+    		const client = new pg.Client(connectionString);
+    		client.connect();
+    		var query = client.query("SELECT senderid from items");
+    		query.on("row", function (row){
+    			requestclasses(row.senderid);
+    			console.log("sent to..." + JSON.stringify(row.senderid));
+    		});
+    		query.on("end", function (result) {          
+        		client.end(); 
+    		});
+			classdatasend(recipientId);
+    		console.log("Got a response");
+  	} else {
+    	console.log("Got an error: ", error, ", status code: ", response.statusCode)
+  	}
+	})
+}
 //send class data
 function classdatasend(recipientId) {
 	
 	var classelements = [];
 	
-	for(i=0; i<classes.length; i++){
-		var classarray = {
-			"title": classes[i].name + " - " + classes[i].instructor_name + " - " + classes[i].start_time,
-			"subtitle": classes[i].description,
-			"image_url": "https://yogaia.com/" + classes[i].instructor_img,
-			"buttons":[{
-				"type": "web_url",
-				"url": "https://yogaia.com/view/" + classes[i].id,
-				"title": "Book"
-			}, {
-				"type": "element_share"
-			}]
-		};
-		classelements.push(classarray);
+	for(var i=0; i<classes.length; i++){
+		if (classes[i].language == "en" && classelements.length < 10){
+			var date = moment(classes[i].start_time, moment.ISO_8601).format("ddd, h:mm A");
+			var classtile = {
+				"title": classes[i].name + " - " + classes[i].instructor_name + " - " + date,
+				"subtitle": classes[i].description,
+				"image_url": "https://yogaia.com/" + classes[i].instructor_img,
+				"buttons":[{
+					"type": "web_url",
+					"url": "https://yogaia.com/view/" + classes[i].id,
+					"title": "Book"
+				}, {
+					"type": "element_share"
+				}]
+			};
+			classelements.push(classtile);
+		}
 	}
+	
+	console.log(classelements);
             
-            
-    message = {
+    var message = {
         "attachment": {
             "type": "template",
             "payload": {
@@ -203,49 +133,36 @@ function classdatasend(recipientId) {
         }
     };
     
+    sendMessage(recipientId, {text: "Good morning! Here's the schedule for the next couple of days, hope you find something you like!"});
     sendMessage(recipientId, message);
 
 }
 
-//var fedesenderid = 1210619582313639;
+new CronJob('* 59 * * * *', function(recipientId) {
+  	//url for classes JSON
+	var url = 'https://yogaia.com/api/lessons?upcoming=1&limit=30';
+	//get JSON, parse it and store it in classes variable
+	request(url, (error, response, body)=> {
+  		if (!error && response.statusCode === 200) {
+  			console.log('Yogaia query successful... storing classes variable');
+    		classes = JSON.parse(body);
+    		console.log('CONNECTING to database');
+   			const connectionString = process.env.DATABASE_URL;
+    		const client = new pg.Client(connectionString);
+    		client.connect();
+    		console.log('sending classes to users...');
+    		var query = client.query("SELECT senderid from items");
+    		query.on("row", function (row){
+    			classdatasend(row.senderid);
+    			console.log("sent to..." + JSON.stringify(row.senderid));
+    		});
+    		query.on("end", function (result) {          
+        		client.end(); 
+    		});
 
-//sendMessage(fedesenderid, {text: "Hello Fede"});
-
-/*
-var job = new CronJob({
-  cronTime: '* * * * * * ',
-  onTick: function() {
-  	classdatasend();
-  	request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-  },
-  start: true
-});
-job.start();
-*/
-
-/*new CronJob('* * * * * *', function(recipientId) {
-  console.log('You will see this message every second');
-    const connectionString = process.env.DATABASE_URL;
-    const client = new pg.Client(connectionString);
-    client.connect();
-    var query = client.query("SELECT senderid from items");
-    query.on("row", function (row){
-    	//sendMessage(row
-    	console.log(JSON.stringify(row.senderid));
-    });
+  	} else {
+    	console.log("Got an error: ", error, ", status code: ", response.statusCode)
+  	}
+	})
   
-}, null, true);*/
+}, null, true);
